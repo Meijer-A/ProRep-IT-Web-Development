931640057ab098b2a80de24285c3b5bdde8f1cba
KoenP <koenpardoen@hotmail.com>
2019-08-07 21:49:44 +0200

    Revert "Possible fix form"
    This reverts commit 81895784e9fe0dcd5abfb6c883eedf69b9d668a6.

--- mail/contact.php
+++ mail/contact.php
@@ -16,7 +16,7 @@ $phone = strip_tags(htmlspecialchars($_POST['phone']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
 	
 // Create the email and send the message
-$to = 'info@prorepit.nl'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$to = 'ayyoubmeijer@hotmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Contact form bericht van:  $name";
 $email_body = "Er is een nieuw bericht van de website https://prorepit.nl/.\n\n"."Hier zijn de details:\n\nNaam: $name\n\nEmail: $email_address\n\nTelefoonnummer: $phone\n\nBericht:\n$message";
 $headers = "From: noreply@prorepit.nl\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
